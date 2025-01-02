ec9f215ea1e2d20c7b904e50c969d328567625ce
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-01-02 09:45:49 -0500

    Add link to cookiecutter doc guide in news file

--- .github/workflows/check-news.py
+++ .github/workflows/check-news.py
@@ -1,6 +1,6 @@
 """Check if the PR has a news item.
 
-Put a warning comment if it doesn't.
+Put a warning comment and check fails if it doesn't.
 """
 
 import os
@@ -59,6 +59,8 @@ def main():
                 """\
 **Warning!** No news item is found for this PR. If this is a user-facing change/feature/fix,
 please add a news item by copying the format from `news/TEMPLATE.rst`.
+For best practices, please visit
+https://Billingegroup.github.io/cookiecutter/cookiecutter-guide.html#news-file-guide.
 """
             )
         assert False
